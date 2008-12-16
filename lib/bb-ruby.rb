45851988af8a033ff1c9034c9cba1b93ae62c8e3
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2008-12-16 12:25:46 -0500

    rdoc updates

--- lib/bb-ruby.rb
+++ lib/bb-ruby.rb
@@ -4,7 +4,10 @@ $:.unshift(File.dirname(__FILE__)) unless
 module BBRuby
   VERSION = '0.8.2'
 
+  # allowable image formats
   @@imageformats = 'png|bmp|jpg|gif|jpeg'
+
+  # built-in BBCode tabs that will be processed
   @@tags = {
     # tag name => [regex, replace, description, example, enable/disable symbol]
     'Bold' => [
@@ -198,49 +201,85 @@ module BBRuby
       :email]
   }
 
-  def self.to_html(text, tags_alternative_definition = {}, escape_html=true, method=:disable, *tags)
-    text = text.clone
-    # escape < and > to remove any html
-    if escape_html 
-      text.gsub!( '<', '&lt;' )
-      text.gsub!( '>', '&gt;' )
+  class << self
+    # Convert a string with BBCode markup into its corresponding HTML markup
+    #
+    # === Basic Usage
+    #
+    # The first parameter is the string off BBCode markup to be processed
+    #
+    #   text = "[b]some bold text to markup[/b]"
+    #   output = BBRuby.to_html(text)
+    #   # output => "<strong>some bold text to markup</strong>"
+    #
+    # === Custom BBCode translations
+    #
+    # You can supply your own BBCode markup translations to create your own custom markup
+    # or override the default BBRuby translations (parameter is a hash of custom translations).
+    #
+    # The hash takes the following format: "name" => [regexp, replacement, description, example, enable_symbol]
+    #
+    #  custom_blockquote = {
+    #    'Quote' => [
+    #      /\[quote(:.*)?=(.*?)\](.*?)\[\/quote\1?\]/mi,
+    #      '<div class="quote"><p><cite>\2</cite></p><blockquote>\3</blockquote></div>',
+    #      'Quote with citation',
+    #      '[quote=mike]please quote me[/quote]',
+    #      :quote
+    #    ]
+    #  }
+    #
+    # === Enable and Disable specific tags
+    #
+    # BBRuby will allow you to only enable certain BBCode tags, or to explicitly disable certain tags.
+    # Pass in either :disable or :enable to set your method, followed by the comma-separated list of tags
+    # you wish to disable or enable
+    #
+    #   BBRuby.to_html(text, {}, true, :enable, :image, :bold, :quote)
+    #   BBRuby.to_html(text, {}, true, :disable, :image, :video, :color)
+    #
+    def to_html(text, tags_alternative_definition = {}, escape_html=true, method=:disable, *tags)
+      text = text.clone
+      # escape < and > to remove any html
+      if escape_html 
+        text.gsub!( '<', '&lt;' )
+        text.gsub!( '>', '&gt;' )
+      end
+      
+      tags_definition = @@tags.merge(tags_alternative_definition)
+
+      # parse bbcode tags
+      case method
+        when :enable
+          tags_definition.each_value { |t| text.gsub!(t[0], t[1]) if tags[0].include?(t[4]) }
+        when :disable
+          # this works nicely because the default is disable and the default set of tags is [] (so none disabled) :)
+          tags_definition.each_value { |t| text.gsub!(t[0], t[1]) unless tags[0].include?(t[4]) }
+      end
+
+      # parse spacing
+      text.gsub!( /\r\n?/, "\n" )
+      text.gsub!( /\n/, "<br />" )
+
+      # return markup
+      text
     end
-    
-    tags_definition = @@tags.merge(tags_alternative_definition)
 
-    # parse bbcode tags
-    case method
-      when :enable
-        tags_definition.each_value { |t| text.gsub!(t[0], t[1]) if tags[0].include?(t[4]) }
-      when :disable
-        # this works nicely because the default is disable and the default set of tags is [] (so none disabled) :)
-        tags_definition.each_value { |t| text.gsub!(t[0], t[1]) unless tags[0].include?(t[4]) }
+    # Returns the list of tags processed by BBRuby in a Hash object
+    def tag_list
+      @@tags
     end
+  end # class << self
 
-    # parse spacing
-    text.gsub!( /\r\n?/, "\n" )
-    text.gsub!( /\n/, "<br />" )
-
-    # return markup
-    text
-  end
-
-  def self.tags
-    @@tags.each { |tn, ti|
-      # yields the tag name, a description of it and example
-      yield tn, ti[2], ti[3] if ti[2]
-    }
-  end
-  
-  def self.tag_list
-    @@tags
-  end
-end
+end # class BBRuby
 
 class String
+  #
   def bbcode_to_html(tags_alternative_definition = {}, escape_html=true, method=:disable, *tags)
     BBRuby.to_html(self, tags_alternative_definition, escape_html, method, tags)
   end
+
+  #
   def bbcode_to_html!(tags_alternative_definition = {}, escape_html=true, method=:disable, *tags)
     self.replace(BBRuby.to_html(self, tags_alternative_definition, escape_html, method, tags))
   end
