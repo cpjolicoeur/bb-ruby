202c39dfdc5b19dcf0cbfd99657bb2ea507cb1ef
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2009-05-21 20:11:28 -0400

    version bump

--- lib/bb-ruby.rb
+++ lib/bb-ruby.rb
@@ -2,7 +2,7 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module BBRuby
-  VERSION = '0.8.4'
+  VERSION = '0.8.5'
 
   # allowable image formats
   @@imageformats = 'png|bmp|jpg|gif|jpeg'
